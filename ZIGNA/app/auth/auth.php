49bdf13005259a970521c0a036daf62d0361fdea
RenataFls <renata.flores.23isc@tecsanpedro.edu.mx>
2026-05-27 17:14:09 -0600

    FIX: auth.php

--- ZIGNA/app/auth/auth.php
+++ ZIGNA/app/auth/auth.php
@@ -1,5 +1,8 @@
 <?php
-session_start();
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 if (!isset($_SESSION['nombre_usuario'])) {
     header("Location: index.php?page=login");
     exit();
