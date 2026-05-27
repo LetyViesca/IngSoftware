41a4ba063215acf2bcc50c0335243f98c03ba793
RenataFls <renata.flores.23isc@tecsanpedro.edu.mx>
2026-05-27 17:47:18 -0600

    FIX: problema de seguridad

--- ZIGNA/app/auth/auth.php
+++ ZIGNA/app/auth/auth.php
@@ -1,10 +1,16 @@
 <?php
+
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
-if (!isset($_SESSION['nombre_usuario'])) {
+
+// Verificar si el usuario inició sesión
+if (!isset($_SESSION['id_usuario'])) {
+
     header("Location: index.php?page=login");
     exit();
 }
+
+// Datos del usuario disponibles globalmente
+$id_usuario = $_SESSION['id_usuario'];
 $nombre_usuario = $_SESSION['nombre_usuario'];
-?>
